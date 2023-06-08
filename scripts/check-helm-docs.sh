e68c64ffd8e9e35f75b8501f62fad9fab9bac4d0
Jaime Pérez Halcón <jhalcon@freepik.com>
2023-06-08 17:05:24 +0200

    ci(ci): fixed version helm-docs

--- scripts/check-helm-docs.sh
+++ scripts/check-helm-docs.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-docker run --rm --volume "$(pwd):/helm-docs" -u "$(id -u)" jnorwood/helm-docs:v1.8.1
+docker run --rm --volume "$(pwd):/helm-docs" -u "$(id -u)" jnorwood/helm-docs:latest
 if ! git diff --exit-code; then
     echo "Documentation not up to date. Please run helm-docs and commit changes!" >&2
     exit 1
