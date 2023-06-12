f0f22b0caa7105a06c5220cc90cbca0e05d1afa7
Jaime Pérez Halcón <jhalcon@freepik.com>
2023-06-12 10:35:01 +0200

    ci(ci): refactor

--- scripts/check-helm-docs.sh
+++ scripts/check-helm-docs.sh
@@ -2,6 +2,6 @@
 
 docker run --rm --volume "$(pwd):/helm-docs" -u "$(id -u)" jnorwood/helm-docs:latest
 if ! git diff --exit-code; then
-    echo "Documentation not up to date. Please run helm-docs and commit changes!" >&2
-    exit 1
+	echo "Documentation not up to date. Please run helm-docs and commit changes!" >&2
+	exit 1
 fi
--- scripts/validate-pr.sh
+++ scripts/validate-pr.sh
@@ -7,31 +7,31 @@ set -o pipefail
 : "${PR_TITLE:?Environment variable must be set}"
 
 main() {
-    local repo_root
-    repo_root=$(git rev-parse --show-toplevel)
+	local repo_root
+	repo_root=$(git rev-parse --show-toplevel)
 
-    local changed
-    changed=$(ct list-changed --config "$repo_root/ct.yaml")
+	local changed
+	changed=$(ct list-changed --config "$repo_root/ct.yaml")
 
-    if [[ -z "$changed" ]]; then
-        exit 0
-    fi
+	if [[ -z "$changed" ]]; then
+		exit 0
+	fi
 
-    local num_changed
-    num_changed=$(wc -l <<< "$changed")
+	local num_changed
+	num_changed=$(wc -l <<< "$changed")
 
-    if ((num_changed > 1)); then
-        echo "This PR has changes to multiple charts. Please create individual PRs per chart." >&2
-        exit 1
-    fi
+	if ((num_changed > 1)); then
+		echo "This PR has changes to multiple charts. Please create individual PRs per chart." >&2
+		exit 1
+	fi
 
-    # Strip charts directory
-    changed="${changed##*/}"
+	# Strip charts directory
+	changed="${changed##*/}"
 
-    if [[ "$PR_TITLE" != "[$changed] "* ]]; then
-        echo "PR title must start with '[$changed] '." >&2
-        exit 1
-    fi
+	if [[ "$PR_TITLE" != "[$changed] "* ]]; then
+		echo "PR title must start with '[$changed] '." >&2
+		exit 1
+	fi
 }
 
 main
